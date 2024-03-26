c0b9bd57f9b75b865a60b26da10015158ce5c3d3
notDecay <thisisjustalongtext@gmail.com>
2024-03-26 23:10:55 +0700

    Add this very cute ascii cat - Global type should be on the "@types" folder - Moves "types.d.ts" to "@types" folder - Cats are the cutest thing in the world :3

--- client/App.tsx
+++ client/App.tsx
@@ -2,6 +2,19 @@ import { HopeProvider } from "@hope-ui/solid"
 import { Router } from "@solidjs/router"
 import * as routes from './routes'
 import { For } from "solid-js"
+import { appConfig } from "public"
+
+console.log(`Starting this app... ${appConfig.version.app}
+  .        ／＞　 フ
+          | 　_　_| 
+        ／\` ミ__^ノ 
+       /　　　　 |
+      /　 ヽ　　 ﾉ              ╱|、
+     /　　 |　|　|            (˚ˎ 。7  
+    ／￣|　　 |　|　|          |、˜〵          
+    (￣ヽ＿_  ヽ_)__)         じしˍ,)ノ
+    ＼二)
+`)
 
 export default function App() {
   return (
--- public/config/index.ts
+++ public/config/index.ts
@@ -1,10 +1,10 @@
 export const appConfig = {
   version: {
-    app: 'v1.0.0-beta1',
+    app: 'v1.0.0-beta-1',
     apiVersion: 'v1.0'
   },
   api: {
     ROUTE: '/duck',
     DEV_ORIGIN: 'http://localhost:4000',
   }
-}
+} as const
